28416f9853d9f11940dd833babc4c32ddfc60c1d
Maruf Maniruzzaman <kuasha@gmail.com>
2015-10-26 06:39:01 +0000

    Added queue functions

--- src/com/lekhoni/basic/Queue.java
+++ src/com/lekhoni/basic/Queue.java
@@ -4,4 +4,37 @@ package com.lekhoni.basic;
  * Created by kuasha on 1/19/15.
  */
 public class Queue {
+    public static final int DefaultSize = 100;
+    
+    private int[] store;
+    private int rear;
+    private int front;
+    
+    public Queue(){
+        store = new int[DefaultSize];
+        rear = front = -1;
+    }
+    
+    public Queue(int size){
+        if(size < 1){
+            throw new IllegalArgumentException("size must be larger than zero.");
+        }
+        
+        this.store = new int[size];
+        rear = front = -1;
+    }
+    
+    public void enqueue(int data){
+        
+    }
+    
+    public int dequeue(){
+        return 0;
+    }
+    
+    public static void main(String[] args){
+        Queue queue = new Queue();
+        queue.enqueue(1);
+        int ret = queue.dequeue();
+    }
 }
