1eddb378ac38d8a5068bf00bd7fde362eb72ea81
Maruf Maniruzzaman <kuasha@gmail.com>
2015-10-28 03:49:42 +0000

    Initial queue implementation

--- src/com/lekhoni/basic/Queue.java
+++ src/com/lekhoni/basic/Queue.java
@@ -9,9 +9,11 @@ public class Queue {
     private int[] store;
     private int rear;
     private int front;
+    private int itemCount = 0;
     
     public Queue(){
         store = new int[DefaultSize];
+        itemCount = 0;
         rear = front = -1;
     }
     
@@ -20,21 +22,55 @@ public class Queue {
             throw new IllegalArgumentException("size must be larger than zero.");
         }
         
-        this.store = new int[size];
+        store = new int[size];
+        itemCount = 0;
         rear = front = -1;
     }
     
     public void enqueue(int data){
+        if (itemCount >= store.length) {
+            throw new IllegalStateException("queue is full");
+        }
         
+        rear++;
+        if (rear == store.length) {
+            rear = 0;
+        }
+        
+        store[rear] = data;
+        itemCount++;
     }
     
     public int dequeue(){
-        return 0;
+        if (itemCount < 1) {
+            throw new IllegalStateException("queue is empty");
+        }
+        
+        front++;
+        if (front > store.length){
+            front = 0;
+        }
+        
+        int data = store[front];
+        
+        itemCount--;
+        
+        return data;
     }
     
     public static void main(String[] args){
         Queue queue = new Queue();
         queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(4);
         int ret = queue.dequeue();
+        System.out.println(ret);
+        ret = queue.dequeue();
+        System.out.println(ret);
+        ret = queue.dequeue();
+        System.out.println(ret);
+        ret = queue.dequeue();
+        System.out.println(ret);
     }
 }
