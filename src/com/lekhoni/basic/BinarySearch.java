d8e0b11b852207a7d97fae1512ba695b537823e2
Maruf Maniruzzaman <kuasha@gmail.com>
2015-10-31 02:39:21 +0000

    Added binary search

--- src/com/lekhoni/basic/BinarySearch.java
+++ src/com/lekhoni/basic/BinarySearch.java
@@ -4,4 +4,31 @@ package com.lekhoni.basic;
  * Created by kuasha on 1/19/15.
  */
 public class BinarySearch {
+    
+    public int search(int[] numbers, int key){
+        int start = 0, end = numbers.length;
+        
+        while(start<=end){
+            int mid = start + (end-start)/2;
+
+            if(numbers[mid]==key){
+                return mid;
+            }
+            else if(numbers[mid]>key){
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        
+        return -1;
+    }
+    
+    public static void main(String[] args){
+        BinarySearch bsearch = new BinarySearch();
+        int[] numbers = {1,5,13,18,34,52,62,74,89};
+        int found = bsearch.search(numbers, 62);
+        System.out.println(found);
+    }
 }
